17af542aabbb1d7fc180a31ff461786131da5d44
Shalini Rallapalli Venkata <rvshalini26@gmail.com>
2020-05-01 16:11:05 -0500

    feature

--- app/login.php
+++ app/login.php
@@ -47,8 +47,8 @@
                 <label>Password:</label>
                 <input type="password" name="password">
                 <br>
-                <input type="submit" value="Submit">
-                <input type="submit" value="Cancel">
+                <input class ="btn btn-default" type="submit" value="Submit">
+                <input class="btn btn-default" type="submit" value="Cancel">
                 <br>
                 <a href="Register">Register Employer/Employee</a>
                 <a href="Forgot Password"> Forgot Password</a>
